f5d60934fbb7c669c095475e16c70c387c71ddfb
Jair Rosa <jair.darosajunior@coinbase.com>
2024-10-25 16:43:20 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import express from 'express'
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    url: process.env.DISCORD_URL,
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
