b30972eb2af693efd19f5f81bd0e1339b50790d5
Jair Rosa <jair.darosajunior@coinbase.com>
2024-10-25 16:45:31 -0300

    initial commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express'
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.status(200).send({
     url: process.env.DISCORD_URL,
   });
